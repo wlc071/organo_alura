df8f0242c5d0c25fe6b14a2ffcec892150cab5bf
Wallace <wallacejose071@gmail.com>
2023-02-09 22:28:05 -0300

    site organo React

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,15 @@
+import { Fragment } from 'react'
+import './Banner.css'
+
+export const Banner = () =>  {
+    // JSX
+    return (
+        <Fragment>
+            <header className="banner">
+                <img src="/imagens/banner.png" alt="Banner principal da página do Organo"/>
+            </header>
+        </Fragment>
+    )
+}
+
+
